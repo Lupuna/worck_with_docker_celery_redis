325278d65ce62c0985f75782a6333a3ab201527c
Lupuna <terragod427@gmail.com>
2024-05-16 17:34:31 +0700

    Part 4 start ORM optimisation

--- service/servises/views.py
+++ service/servises/views.py
@@ -1,9 +1,14 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from clients.models import Client
 from servises.models import Subscription
 from servises.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # queryset = Subscription.objects.all().prefetch_related('client', 'client__user') достает ненужные поля
+    queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+    )
     serializer_class = SubscriptionSerializer
-
