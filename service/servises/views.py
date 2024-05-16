d286ef19e415bc9082a59029928c81338b4c9821
Lupuna <terragod427@gmail.com>
2024-05-16 16:37:00 +0700

    Part 3 complete base project settings

--- service/servises/views.py
+++ service/servises/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from servises.models import Subscription
+from servises.serializers import SubscriptionSerializer
 
-# Create your views here.
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
